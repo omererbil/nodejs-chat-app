e21d63a33e5f551fa946e7c2609842e4936fdda4
Govan <govan1102@gmail.com>
2017-10-31 13:35:20 +0000

    add newMessage and createMessage

--- server/server.js
+++ server/server.js
@@ -12,6 +12,16 @@ app.use(express.static(publicPath));
 io.on('connection',(socket) =>{
     console.log("new user connected");
     
+    socket.emit('newMessage',{
+        from:'dana',
+        text:'hey, its new message',
+        createdAt:'123'
+    });
+    
+    socket.on('createMessage',(message) =>{
+        console.log('createMessage', message)
+    })
+    
     socket.on('disconnect',() =>{
     console.log("user has disconnected")
     })
