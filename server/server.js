04978ce7048c4aca57328d3b67a38760e0be47f7
Govan <govan1102@gmail.com>
2017-11-02 16:24:02 +0000

    add geolocation

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const publicPath=path.join(__dirname,'../public');
 const http=require("http")
 const express=require("express");
 const socketIO=require("socket.io")
-const{generateMessage}=require("./utils/message")
+const{generateMessage,generateLocationMessage}=require("./utils/message")
 var app=express(); 
 var server=http.createServer(app);
 var io=socketIO(server)
@@ -25,6 +25,10 @@ io.on('connection',(socket) =>{
     
     })
     
+    
+    socket.on('createLocationMessage', (coords) => {
+    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+  })
     socket.on('disconnect',() =>{
     console.log("user has disconnected")
     });
